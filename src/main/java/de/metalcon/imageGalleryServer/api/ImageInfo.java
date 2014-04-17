1b3cec9c8922870f0c9f8e9b64a95d8e018ab53f
Sebastian Schlicht <sebschlicht@uni-koblenz.de>
2014-04-17 23:02:01 +0200

    ImageInfo now class for serialization

--- src/main/java/de/metalcon/imageGalleryServer/api/ImageInfo.java
+++ src/main/java/de/metalcon/imageGalleryServer/api/ImageInfo.java
@@ -6,34 +6,67 @@ package de.metalcon.imageGalleryServer.api;
  * @author sebschlicht
  * 
  */
-public interface ImageInfo {
+public class ImageInfo {
+
+    protected long timestamp;
+
+    protected long identifier;
+
+    protected String urlLink;
+
+    protected String title;
+
+    protected String metaData;
+
+    public ImageInfo(
+            long timestamp,
+            long identifier,
+            String urlLink,
+            String title,
+            String metaData) {
+        this.timestamp = timestamp;
+        this.identifier = identifier;
+        this.urlLink = urlLink;
+        this.title = title;
+        this.metaData = metaData;
+    }
 
     /**
      * @return timestamp of image upload
      */
-    public long getTimestamp();
+    public long getTimestamp() {
+        return timestamp;
+    }
 
     /**
      * @return image identifier
      */
-    public long getIdentifier();
+    public long getIdentifier() {
+        return identifier;
+    }
 
     /**
      * @return URL image refers to<br>
      *         (optional, may be <b>null</b>)
      */
-    public String getUrlLink();
+    public String getUrlLink() {
+        return urlLink;
+    }
 
     /**
      * @return image title<br>
      *         (optional, may be <b>null</b>)
      */
-    public String getTitle();
+    public String getTitle() {
+        return title;
+    }
 
     /**
      * @return additional meta data<br>
      *         (optional, may be <b>null</b>)
      */
-    public String getMetaData();
+    public String getMetaData() {
+        return metaData;
+    }
 
 }
